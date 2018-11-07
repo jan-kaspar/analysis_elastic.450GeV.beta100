abc4b243a9b398c58467656ad2f4899879666304
Jan Kašpar <jan.kaspar@cern.ch>
2018-11-07 13:45:09 +0100

    Added new dataset.

---
+++ DS-fill7284/EmptyBX/input_files.h
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+//----------------------------------------------------------------------------------------------------
+
+std::vector<std::string> input_files;
+
+void InitInputFiles()
+{
+	input_files = {
+		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta100/EmptyBX/version1/run_324485.0_re_reco_EmptyBX.root",
+		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta100/EmptyBX/version1/run_324485.10_re_reco_EmptyBX.root",
+		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta100/EmptyBX/version1/run_324485.1_re_reco_EmptyBX.root",
+		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta100/EmptyBX/version1/run_324485.2_re_reco_EmptyBX.root",
+		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta100/EmptyBX/version1/run_324485.3_re_reco_EmptyBX.root",
+		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta100/EmptyBX/version1/run_324485.4_re_reco_EmptyBX.root",
+		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta100/EmptyBX/version1/run_324485.5_re_reco_EmptyBX.root",
+		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta100/EmptyBX/version1/run_324485.6_re_reco_EmptyBX.root",
+		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta100/EmptyBX/version1/run_324485.7_re_reco_EmptyBX.root",
+		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta100/EmptyBX/version1/run_324485.8_re_reco_EmptyBX.root",
+		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta100/EmptyBX/version1/run_324485.9_re_reco_EmptyBX.root"
+	};
+}
---
+++ DS-fill7284/EmptyBX/parameters.h
@@ -0,0 +1,35 @@
+#include "parameters_global.h"
+
+unsigned int timestamp_min = 206071;
+unsigned int timestamp_max = 218141;
+
+void Init_base()
+{
+	// load global settings
+	Init_global();
+
+	keepAllBunches = false;
+	bunchMap[324485] = { 462, 520 };
+
+	//env.UseMatchedOptics();
+}
+
+//----------------------------------------------------------------------------------------------------
+
+void Init_45b_56t()
+{
+	Init_global_45b_56t();
+
+	// normalisation settings
+	anal.L_int = 1.;
+}
+
+//----------------------------------------------------------------------------------------------------
+
+void Init_45t_56b()
+{
+	Init_global_45t_56b();
+
+	// normalisation settings
+	anal.L_int = 1.;
+}
