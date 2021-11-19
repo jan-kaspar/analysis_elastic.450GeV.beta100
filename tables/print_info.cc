a041f897e8d2b8eadf44c4235c02edd5a4ec1768
Jan Kaspar <jan.kaspar@cern.ch>
2021-11-19 12:06:15 +0100

    print more tables

--- tables/print_info.cc
+++ tables/print_info.cc
@@ -1,27 +1,98 @@
 #include "TFile.h"
 #include "TH1D.h"
 
+#include <cstdio>
 #include <vector>
 
 using namespace std;
 
 //----------------------------------------------------------------------------------------------------
 
+double GetEventsInRange(const TH1D *h, double t_min, double t_max)
+{
+	double sum = 0.;
+	for (int bi = 1; bi <= h->GetNbinsX(); ++bi)
+	{
+		const double t = h->GetBinCenter(bi);
+		if (t_min <= t && t < t_max)
+			sum += h->GetBinContent(bi);
+	}
+
+	return sum;
+}
+
+//----------------------------------------------------------------------------------------------------
+
+void PrintStats(const vector<string> &datasets, const vector<string> &diagonals, const string &type, double t_min, double t_max)
+{
+	string obj_path = "";
+	if (type == "tagged") obj_path = "acceptance correction/sb1/h_t_Nev_before";
+	if (type == "accepted") obj_path = "acceptance correction/sb1/h_t_Nev_after_no_corr";
+
+	if (obj_path.empty())
+	{
+		printf("ERROR: type '%s' not recognized.\n", type.c_str());
+		return;
+	}
+
+	printf("\n\n* %s events in |t| range from %.3E to %.3E\n", type.c_str(), t_min, t_max);
+	
+	printf("%30s", "dataset");
+	for (const auto &diagonal : diagonals)
+		printf("%13s", diagonal.c_str());
+	printf("\n");
+
+	double entries_tot = 0.;
+
+	for (const auto &dataset : datasets)
+	{
+		printf("%30s", dataset.c_str());
+
+		for (const auto &diagonal : diagonals)
+		{
+			const string f_in_name = "../data/" + dataset + "/distributions_" + diagonal + ".root";
+			TFile *f_in = TFile::Open(f_in_name.c_str());
+
+			TH1D *h_in = (TH1D *) f_in->Get(obj_path.c_str());
+
+			// remove
+			//const double entries = h_in->GetEntries();
+			const double entries = GetEventsInRange(h_in, t_min, t_max);
+
+			printf("%13.3E", entries);
+
+			entries_tot += entries;
+
+			delete f_in;
+		}
+
+		printf("\n");
+	}
+
+	printf("%30s\n", "----------");
+	printf("%30s%13.3E\n", "total (datasets, diagonals)", entries_tot);
+}
+
+//----------------------------------------------------------------------------------------------------
+
 int main()
 {
 	vector<string> datasets = {
-		"DS-fill7280/Totem1",
-		"DS-fill7281/Totem1",
-		"DS-fill7282/Totem1",
-		"DS-fill7283/Totem1",
-		"DS-fill7284/Totem1",
-		"DS-fill7285/Totem1",
-		"DS-fill7286/Totem1",
-		"DS-fill7287/Totem1",
-		"DS-fill7288/Totem1",
-		"DS-fill7289/Totem1",
-		"DS-fill7290/Totem1",
-		"DS-fill7291/Totem1",
+		"fill7280/Totem1",
+		"fill7281/Totem1",
+		"fill7281-with-hor/Totem1",
+		"fill7282/Totem1",
+		"fill7283/Totem1",
+		"fill7284/Totem1",
+		"fill7285/Totem1",
+		//"fill7286/Totem1",
+		//"fill7287/Totem1",
+		//"fill7288/Totem1",
+		"fill7289/Totem1",
+		"fill7289-with-hor/Totem1",
+		//"fill7290/Totem1",
+		"fill7291/Totem1",
+		"fill7291-with-hor/Totem1",
 	};
 
 	vector<string> diagonals = {
@@ -29,36 +100,11 @@ int main()
 		"45t_56b",
 	};
 
-	double entries_tot = 0.;
-	
-	printf("%20s", "dataset");
-	for (const auto &diagonal : diagonals)
-		printf("%10s", diagonal.c_str());
-	printf("\n");
+	PrintStats(datasets, diagonals, "tagged", 0., 1E4);
 
-	for (const auto &dataset : datasets)
-	{
-		printf("%20s", dataset.c_str());
-
-		for (const auto &diagonal : diagonals)
-		{
-			string f_in_name = "../" + dataset + "/distributions_" + diagonal + ".root";
-			TFile *f_in = TFile::Open(f_in_name.c_str());
-
-			string h_path = "elastic cuts/cut 1/h_cq1";
-			TH1D *h_in = (TH1D *) f_in->Get(h_path.c_str());
-
-			double entries = h_in->GetEntries();
-
-			printf("%10.3E", entries);
-
-			entries_tot += entries;
-		}
-
-		printf("\n");
-	}
-
-	printf("\n* total: %.3E\n", entries_tot);
+	PrintStats(datasets, diagonals, "accepted", 3E-4, 0.005);
+	PrintStats(datasets, diagonals, "accepted", 0.005, 0.010);
+	PrintStats(datasets, diagonals, "accepted", 0.010, 1E4);
 
 	return 0;
 }
