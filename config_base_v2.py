64f55f2b49ffc2ca587f8373c9e6e072e4598b19
Jan Kaspar <jan.kaspar@cern.ch>
2021-12-13 12:14:09 +0100

    base config v3

--- config_base_v2.py
+++ config_base_v2.py
@@ -24,7 +24,7 @@ class ShrinkSigmas:
     self.x = x
     self.y = y
 
-shrink_sigmas_def = ShrinkSigmas(13E-6, 4E-6)
+shrink_sigmas_def = ShrinkSigmas(16E-6, 4E-6)
 
 def Shrink(points, sigma_x = -1, sigma_y = -1, th_x_low = -100E-6, th_x_high = +100E-6, th_y_low = 50E-6, th_y_high = 100E-6):
   if sigma_x < 0:
@@ -239,13 +239,13 @@ cfg = cms.PSet(
     fc_R = FiducialCut([]),
     fc_G = FiducialCut([]),
 
-    vtx_x_min = cms.double(-1.3),
-    vtx_x_max = cms.double(+1.3),
+    vtx_x_min = cms.double(-1E100),
+    vtx_x_max = cms.double(+1E100),
     vtx_y_min = cms.double(-1E100),
     vtx_y_max = cms.double(+1E100),
 
     th_x_default = cms.double(0E-6),
-    th_x_safe_max = cms.double(300E-6),
+    th_x_safe_max = cms.double(10000E-6),
 
     si_th_x_1arm_L = cms.double(0.),
     si_th_x_1arm_R = cms.double(0.),
@@ -327,7 +327,7 @@ def SetResolutions(cfg, si_th_x_1arm, si_th_y_1arm):
   cfg.anal.si_th_y_2arm = si_th_y_1arm / math.sqrt(2.)
   cfg.anal.si_th_y_2arm_unc = -999.
 
-SetResolutions(cfg, 26E-6 / math.sqrt(2), 7.9E-6 / math.sqrt(2))
+SetResolutions(cfg, 23E-6, 5.87E-6)
 
 #----------------------------------------------------------------------------------------------------
 
@@ -339,45 +339,34 @@ a_R_2_F, b_R_2_F, c_R_2_F = +6.0E-3, +863E-3,   -0E-3
 
 #----------------------------------------------------------------------------------------------------
 
-alignment_45b_56t = {
-  "L_2_F" : { "a": a_L_2_F + 2E-3 - 0.3E-3, "b": b_L_2_F - 17E-3 + 1.7E-3, "c": c_L_2_F - 11E-3 - 90E-3 + 36E-3 },
-  "L_1_F" : { "a": a_L_1_F + 2E-3 + 0.3E-3, "b": b_L_1_F + 17E-3 - 1.7E-3, "c": c_L_1_F + 11E-3 - 90E-3 + 36E-3 },
-  "R_1_F" : { "a": a_R_1_F - 2E-3 + 1.3E-3, "b": b_R_1_F -  3E-3 - 1.7E-3, "c": c_R_1_F + 30E-3 - 90E-3 + 36E-3 },
-  "R_2_F" : { "a": a_R_2_F - 2E-3 - 1.3E-3, "b": b_R_2_F +  3E-3 + 1.7E-3, "c": c_R_2_F - 30E-3 - 90E-3 + 36E-3 },
-}
-
 single_arm_contour_45b_56t_full = [[-300E-6, 34E-6], [50E-6, 34E-6], [245E-6, 45E-6], [365E-6, 80E-6], [270E-6, 126E-6], [-280E-6, 131E-6], [-370E-6, 60E-6]]
-single_arm_contour_45b_56t = CutContour(single_arm_contour_45b_56t_full, +305E-6, 0E-6, +304E-6, 150E-6)
-single_arm_contour_45b_56t = CutContour(single_arm_contour_45b_56t, -362E-6, 150E-6, -363E-6, 0E-6)
+single_arm_contour_45b_56t = CutContour(single_arm_contour_45b_56t_full, +220E-6, 0E-6, +221E-6, 150E-6)
+single_arm_contour_45b_56t = CutContour(single_arm_contour_45b_56t, 1., 115E-6, -1., 115E-6)
+
 double_arm_contour_45b_56t = Shrink(single_arm_contour_45b_56t, th_x_high=+219E-6, th_y_high=114E-6)
+
+alignment_45b_56t = {
+  "L_2_F" : { "a": a_L_2_F + 2E-3 - 0.3E-3, "b": b_L_2_F - 17E-3, "c": c_L_2_F - 11E-3 - 90E-3 },
+  "L_1_F" : { "a": a_L_1_F + 2E-3 + 0.3E-3, "b": b_L_1_F + 17E-3, "c": c_L_1_F + 11E-3 - 90E-3 },
+  "R_1_F" : { "a": a_R_1_F - 2E-3 + 1.3E-3, "b": b_R_1_F -  3E-3, "c": c_R_1_F + 30E-3 - 90E-3 },
+  "R_2_F" : { "a": a_R_2_F - 2E-3 - 1.3E-3, "b": b_R_2_F +  3E-3, "c": c_R_2_F - 30E-3 - 90E-3 },
+}
 
 cfg_45b_56t = cfg.clone(
   anal = dict(
     alignment_sources = FormatAlignment(alignment_45b_56t),
 
-    post_reco_adjustment_L = dict(
-      B = +30.7E-6 - 6.0E-6,
-      C = +214,
-      F = +5.5E-3
-    ),
+    cut1_a = 1., cut1_c = -1.91E-6, cut1_si = 35E-6,
+    cut2_a = 1., cut2_c = +0.53E-6, cut2_si = 8E-6,
 
-    post_reco_adjustment_R = dict(
-      B = -29.0E-6 + 6.0E-6,
-      C = -150,
-      F = -5.5E-3
-    ),
-
-    cut1_a = 1., cut1_c = -0.18E-6, cut1_si = 29E-6,
-    cut2_a = 1., cut2_c = +0.09E-6, cut2_si = 8E-6,
-
-    cut5_a = 0.105, cut5_c = -0.015, cut5_si = 0.08,
+    cut5_a = 0.105, cut5_c = -0.01, cut5_si = 0.08,
     cut6_a = 0.105, cut6_c = +0.03, cut6_si = 0.08,
 
-    cut7_a = 0., cut7_c = +0.00, cut7_si = 0.53,
-    cut8_a = 0., cut8_c = -0.21, cut8_si = 2.8,
+    cut7_a = 0., cut7_c = +0.01, cut7_si = 0.55,
+    cut8_a = 0., cut8_c = -0.23, cut8_si = 2.8,
 
-    cut9_a = -0.28, cut9_c = -0.007, cut9_si = 0.14,
-    cut10_a = -0.29, cut10_c = +0.001, cut10_si = 0.14,
+    cut9_a = -0.28, cut9_c = +0.00, cut9_si = 0.14,
+    cut10_a = -0.29, cut10_c = +0.01, cut10_si = 0.14,
 
     fc_L = FiducialCut(single_arm_contour_45b_56t),
     fc_R = FiducialCut(single_arm_contour_45b_56t),
@@ -390,45 +379,34 @@ cfg_45b_56t = cfg.clone(
 
 #----------------------------------------------------------------------------------------------------
 
-alignment_45t_56b = {
-  "L_2_F" : { "a": a_L_2_F - 0.3E-3 + 1.5E-3, "b": b_L_2_F - 15E-3, "c": c_L_2_F + 15E-3 + 70E-3 - 153E-3},
-  "L_1_F" : { "a": a_L_1_F - 0.3E-3 - 1.5E-3, "b": b_L_1_F + 15E-3, "c": c_L_1_F - 15E-3 + 70E-3 - 153E-3},
-  "R_1_F" : { "a": a_R_1_F + 0.3E-3 + 1.3E-3, "b": b_R_1_F + 10E-3, "c": c_R_1_F + 20E-3 + 70E-3 - 153E-3},
-  "R_2_F" : { "a": a_R_2_F + 0.3E-3 - 1.3E-3, "b": b_R_2_F - 10E-3, "c": c_R_2_F - 20E-3 + 70E-3 - 153E-3},
-}
-
 single_arm_contour_45t_56b_full = [[-50E-6, 34E-6], [-240E-6, 45E-6], [-360E-6, 80E-6], [-270E-6, 132E-6], [250E-6, 134E-6], [360E-6, 95E-6], [370E-6, 42E-6], [330E-6, 34E-6]]
-single_arm_contour_45t_56b = CutContour(single_arm_contour_45t_56b_full, +363E-6, 0E-6, +362E-6, 150E-6)
-single_arm_contour_45t_56b = CutContour(single_arm_contour_45t_56b, -332E-6, 150E-6, -333E-6, 0E-6)
+single_arm_contour_45t_56b = CutContour(single_arm_contour_45t_56b_full, -220E-6, 150E-6, -221E-6, 0E-6)
+single_arm_contour_45t_56b = CutContour(single_arm_contour_45t_56b, 1., 115E-6, -1., 115E-6)
+
 double_arm_contour_45t_56b = Shrink(single_arm_contour_45t_56b, th_x_low=-219E-6, th_y_high=114E-6)
+
+alignment_45t_56b = {
+  "L_2_F" : { "a": a_L_2_F - 0.3E-3 + 1.5E-3, "b": b_L_2_F - 15E-3, "c": c_L_2_F + 15E-3 + 70E-3},
+  "L_1_F" : { "a": a_L_1_F - 0.3E-3 - 1.5E-3, "b": b_L_1_F + 15E-3, "c": c_L_1_F - 15E-3 + 70E-3},
+  "R_1_F" : { "a": a_R_1_F + 0.3E-3 + 1.3E-3, "b": b_R_1_F + 10E-3, "c": c_R_1_F + 20E-3 + 70E-3},
+  "R_2_F" : { "a": a_R_2_F + 0.3E-3 - 1.3E-3, "b": b_R_2_F - 10E-3, "c": c_R_2_F - 20E-3 + 70E-3},
+}
 
 cfg_45t_56b = cfg.clone(
   anal = dict(
     alignment_sources = FormatAlignment(alignment_45t_56b),
 
-    post_reco_adjustment_L = dict(
-      B = +33.4E-6 - 8.3E-6,
-      C = +150,
-      F = +8.5E-3
-    ),
+    cut1_a = 1., cut1_c = -0.47E-6, cut1_si = 34E-6,
+    cut2_a = 1., cut2_c = -1.12E-6, cut2_si = 8E-6,
 
-    post_reco_adjustment_R = dict(
-      B = -29.3E-6 + 8.3E-6,
-      C = -305,
-      F = -8.5E-3
-    ),
+    cut5_a = 0.105, cut5_c = +0.01, cut5_si = 0.08,
+    cut6_a = 0.105, cut6_c = -0.04, cut6_si = 0.08,
 
-    cut1_a = 1., cut1_c = -0.05E-6, cut1_si = 29E-6,
-    cut2_a = 1., cut2_c = -0.00E-6, cut2_si = 8E-6,
+    cut7_a = 0., cut7_c = -0.01, cut7_si = 0.55,
+    cut8_a = 0., cut8_c = +0.12, cut8_si = 2.8,
 
-    cut5_a = 0.105, cut5_c = +0.03, cut5_si = 0.08,
-    cut6_a = 0.105, cut6_c = -0.02, cut6_si = 0.08,
-
-    cut7_a = 0., cut7_c = -0.01, cut7_si = 0.53,
-    cut8_a = 0., cut8_c = +0.13, cut8_si = 2.8,
-
-    cut9_a = -0.28, cut9_c = +0.00, cut9_si = 0.14,
-    cut10_a = -0.29, cut10_c = -0.01, cut10_si = 0.14,
+    cut9_a = -0.28, cut9_c = +0.01, cut9_si = 0.14,
+    cut10_a = -0.29, cut10_c = -0.00, cut10_si = 0.14,
 
     fc_L = FiducialCut(single_arm_contour_45t_56b),
     fc_R = FiducialCut(single_arm_contour_45t_56b),
